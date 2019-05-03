d1925c0fd38828772d64bc1bba06bd06185e9f88
Xinran Che <gracexinran@gmail.com>
2019-05-03 21:05:57 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,3 +1,10 @@
 def turn_count(board)
-  board.length.each do |count|
-    if 
+  count = 0 
+  board.length.each do |element|
+    if element == "X" || element == "O"
+      count += 1
+    end
+  end
+end
+
+def current_player()
